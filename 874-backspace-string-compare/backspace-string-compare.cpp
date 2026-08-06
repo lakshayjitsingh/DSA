05543855e1de34aa32192c2692c256fb26136b46
lakshayjitsingh <81737609+lakshayjitsingh@users.noreply.github.com>
2026-08-06 11:58:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (48.70%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,11 +1,48 @@
 class Solution {
 public:
-bool backspaceCompare(string s, string t) {
-auto process=[](const string& str){
-string res;
-for(char c:str) { if(c=='#'){if(!res.empty())res.pop_back();} else res+=c; }
-return res;
-};
-return process(s)==process(t);
-}
+    bool backspaceCompare(string s, string t) {
+        int i = s.size() - 1;
+        int j = t.size() - 1;
+        int skipS = 0;
+        int skipT = 0;
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skipS++;
+                    i--;
+                }
+                else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                }
+                else {
+                    break;
+                }
+            }
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skipT++;
+                    j--;
+                }
+                else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                }
+                else {
+                    break;
+                }
+            }
+            if (i >= 0 && j >= 0) {
+
+                if (s[i] != t[j])
+                    return false;
+            }
+            else if (i >= 0 || j >= 0) {
+                return false;
+            }
+            i--;
+            j--;
+        }
+        return true;
+    }
 };
