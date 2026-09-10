8068fea54f46d94f0c7a95ba762c71fd50252123
lakshayjitsingh <81737609+lakshayjitsingh@users.noreply.github.com>
2026-09-10 12:31:46 +0530

    Time: 0 ms (100.00%) | Memory: 24.6 MB (28.52%) - LeetSync

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> st;
+        while (root != NULL || !st.empty()) {
+            while (root != NULL) {
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            st.pop();
+            k--;
+            if (k == 0)
+                return root->val;
+            root = root->right;
+        }
+        return -1;
+    }};
